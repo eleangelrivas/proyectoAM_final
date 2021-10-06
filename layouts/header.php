7b5b50808ec250232e9af5b02a42b36f934e1eee
Ele <ar09017@ues.edu.sv>
2021-10-06 08:52:39 -0600

    Modificacion login

--- layouts/header.php
+++ layouts/header.php
@@ -1,12 +1,12 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-        <title>Admiria - Responsive Flat Admin Dashboard</title>
-        <meta content="Admin Dashboard" name="description" />
-        <meta content="Themesbrand" name="author" />
+        <title>Proyecto - AM</title>
+        <meta content="Curso programación PHP AM" name="description" />
+        <meta content="Elenilson" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
         <!-- App Icons -->
