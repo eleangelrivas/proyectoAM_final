5fca9ccffc47af98d369d869ffd65907de30372f
Ele <ar09017@ues.edu.sv>
2021-10-07 08:57:22 -0600

    prueba conexion

--- Conexion/Conexion.php
+++ Conexion/Conexion.php
@@ -1,6 +1,9 @@
 <?php 
     
-    include_once("login.php");
+    define("HOSTNAME", "localhost");// Nombre del host
+    define("DATABASE", "ejemplo1"); // Nombre de la base de datos
+    define("USERNAME", "ele1990"); // Nombre del usuario
+    define("PASSWORD", "elenilson"); 
     /**
      * summary
      */
--- Conexion/login.php
+++ Conexion/login.php
@@ -1,6 +1,6 @@
 <?php 
 	define("HOSTNAME", "localhost");// Nombre del host
-	define("DATABASE", "telares"); // Nombre de la base de datos
+	define("DATABASE", "ejemplo1"); // Nombre de la base de datos
 	define("USERNAME", "ele1990"); // Nombre del usuario
 	define("PASSWORD", "elenilson"); 
 ?>
